047ea034a2bb7bb5c5994039e8557227010b0be4
Gonçalo Andrade <goncalo.s.and@gmail.com>
2020-01-29 14:16:26 +0000

    add more questions

--- settings.py
+++ settings.py
@@ -1,18 +1,100 @@
 QUESTIONS = [
     {
-        "question": "Is it true 1?",
-        "A": "A",
-        "B": "B",
-        "C": "C",
-        "D": "D",
+        "question": "A personagem de cinema \"John Rambo\" \nfoi interpretada por que ator?",
+        "A": "Vin Diesel",
+        "B": "Sylvester Stallone",
+        "C": "Arnold Schwarzenegger",
+        "D": "Van Damme",
         "solution": "B"
     },
     {
-        "question": "Is it true 2?",
-        "A": "A",
-        "B": "B",
-        "C": "C",
-        "D": "D",
+        "question": "Quantos meses tem um seculo?",
+        "A": "1000",
+        "B": "120",
+        "C": "60",
+        "D": "1200",
+        "solution": "D"
+    },
+    {
+        "question": "Quantos quilos tem uma tonelada?",
+        "A": "10000",
+        "B": "600",
+        "C": "1000",
+        "D": "100",
         "solution": "C"
     },
+    {
+        "question": "O que é um Paleontólogo?",
+        "A": "Pessoa que não estuda",
+        "B": "Pessoa que estuda plantas",
+        "C": "Pessoa que estuda fósseis",
+        "D": "Pessoa que fabrixa palitos",
+        "solution": "C"
+    },
+    {
+        "question": "O que significa PIB?",
+        "A": "Pequeno Índice de Burrice",
+        "B": "Potencial Individual de Base",
+        "C": "Preços de Importação Baratos",
+        "D": "Produto Interno Bruto",
+        "solution": "D"
+    },
+    {
+        "question": "Que nome se dá à camada gordurosa \nque se forma à superfície do leite?",
+        "A": "Café",
+        "B": "Queijo",
+        "C": "Iogurte",
+        "D": "Nata",
+        "solution": "D"
+    },
+    {
+        "question": "Quantos anos tem uma década?",
+        "A": "5",
+        "B": "100",
+        "C": "1",
+        "D": "10",
+        "solution": "D"
+    },
+    {
+        "question": "Quantas patas tem um cão?",
+        "A": "6",
+        "B": "2",
+        "C": "3",
+        "D": "4",
+        "solution": "D"
+    },
+    {
+        "question": "Qual é a capital da Rússia?",
+        "A": "São Petesburgo",
+        "B": "Ecaterimburgo",
+        "C": "Moscovo",
+        "D": "Samara",
+        "solution": "C"
+    },
+    {
+        "question": "Que cidade norte-americana \né conhecida como a capital do jogo?",
+        "A": "Nova Iorque",
+        "B": "São Francisco",
+        "C": "Dallas",
+        "D": "Las Vegas",
+        "solution": "D"
+    },
+]
+
+PRIZES = [
+    50,
+    100,
+    200,
+    300,
+    500,
+    700,
+    1200,
+    2000,
+    3000,
+    4000,
+    6000,
+    12000,
+    20000,
+    30000,
+    100000,
 ]
