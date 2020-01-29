be17ad782fd01eae08e2ccbb802a5c399724db52
Gonçalo Andrade <goncalo.s.and@gmail.com>
2020-01-29 13:34:39 +0000

    add other question

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
 
 # kivy modules:
 from kivy.app import App
+from kivy import properties as kp
     # uix:
 from kivy.uix.screenmanager import ScreenManager, NoTransition, Screen
 from kivy.uix.relativelayout import RelativeLayout
+# self modules:
+from settings import QUESTIONS
 
+
+class Game(Screen):
+    question = kp.StringProperty()
+    A = kp.StringProperty()
+    B = kp.StringProperty()
+    C = kp.StringProperty()
+    D = kp.StringProperty()
+    current_question = 0
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.update_question()
+
+    def update_question(self):
+        question_dict = QUESTIONS[self.current_question]
+        self.question = question_dict.get("question")
+        self.A = question_dict.get("A")
+        self.B = question_dict.get("B")
+        self.C = question_dict.get("C")
+        self.D = question_dict.get("D")
+        self.solution = question_dict.get("solution")
+
+    def check(self, *args):
+        print("check", args)
+        answer = args[0]
+        if answer == self.solution:
+            print("correct")
+            self.current_question += 1
+            if self.current_question == len(QUESTIONS):
+                print("YOU WIN")
+                return
+        else:
+            print("incorrect")
+            self.current_question = 0
+        self.update_question()
 
 
 class GameApp(App):
 
     def build(self):
-        self.game = Screen()
+        self.game = Game()
         return self.game
 
 
---
+++ settings.py
@@ -0,0 +1,18 @@
+QUESTIONS = [
+    {
+        "question": "Is it true 1?",
+        "A": "A",
+        "B": "B",
+        "C": "C",
+        "D": "D",
+        "solution": "B"
+    },
+    {
+        "question": "Is it true 2?",
+        "A": "A",
+        "B": "B",
+        "C": "C",
+        "D": "D",
+        "solution": "C"
+    },
+]
